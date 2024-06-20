343a4b1c4e1e8328f347f6a15bdf4790045df179
atulgupta0403 <atulgupta0403@gmail.com>
2024-06-21 01:00:13 +0530

    Navbar done

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import "./navbar.css"
 import { Link } from 'react-router-dom'
+// import ReactDOM from 'react-dom'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faHouse } from '@awesome.me/kit-KIT_CODE/icons/classic/solid'
 
 const navbar = () => {
 
@@ -8,15 +11,14 @@ const navbar = () => {
   return (
     <div className='nav'>
       <div className="logo">
-        {/* <img src="https://images-platform.99static.com//2CeMN8mCktTKwCiGN1a3DLEuwkE=/0x0:1000x1000/fit-in/500x500/99designs-contests-attachments/139/139945/attachment_139945363" alt="" /> */}
+        <img src="https://images-platform.99static.com//2CeMN8mCktTKwCiGN1a3DLEuwkE=/0x0:1000x1000/fit-in/500x500/99designs-contests-attachments/139/139945/attachment_139945363" alt="" />
       </div>
       <div className="sections">
-        <p onClick={()=>{setMenu("Home")}}><Link to="/">Home</Link>
-            <span></span>
-        </p>
-        <p onClick={()=>{setMenu("About")}}><Link to="/about">About</Link></p>
-        <p onClick={()=>{setMenu("Project")}}><Link to="/project">Project</Link></p>
-        <p onClick={()=>{setMenu("Contact")}}><Link to="/contact">Contact</Link></p>
+        {/* <FontAwesomeIcon icon={faBars} /> */}
+        <p onClick={()=>{setMenu("Home")}}><Link to="/">Home </Link> {menu === "Home"? <span></span>:<></>} </p>
+        <p onClick={()=>{setMenu("About")}}><Link to="/about">About</Link>  {menu === "About"? <span></span>:<></>}</p>
+        <p onClick={()=>{setMenu("Project")}}><Link to="/project">Project</Link>  {menu === "Project"? <span></span>:<></>} </p>
+        <p onClick={()=>{setMenu("Contact")}}><Link to="/contact">Contact</Link>  {menu === "Contact"? <span></span>:<></>}</p>
       </div>
 
     </div>
