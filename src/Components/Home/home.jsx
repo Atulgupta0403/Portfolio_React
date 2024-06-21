feb2072394c8cc4270de9989328f115da1e06d49
atulgupta0403 <atulgupta0403@gmail.com>
2024-06-21 21:29:47 +0530

    home

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./home.css"
+import home_image from "../Assets/home-main.svg"
 
 const home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='home'>
+      <div className="home-left">
+        <h1>INTRODUCTION</h1>
+        <p>Hii!! My Name is <span className="design">Atul Gupta </span>, I pursuing my B.tech from <span className="design">AJAY KUMAR GARG</span> engineering college 1st year CSE branch. My is to listen song , playing Guitar and coding.. </p>
+      </div>
+      <div className="home-right">
+        <img src={home_image} alt="" />
+      </div>
+      
     </div>
   )
 }
